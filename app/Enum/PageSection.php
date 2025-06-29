e3ae26dd3bde410285c27223e36fd9b4ecea9b07
Rhishi-kesh <reshikash300@gmail.com>
2025-06-29 18:36:25 +0600

    Our Value Section Seeder

--- app/Enum/PageSection.php
+++ app/Enum/PageSection.php
@@ -8,4 +8,5 @@ enum PageSection: string
     case ABOUT_SECTION = "about_section";
     case ABOUT_SECTION_INFO = "about_section_info";
     case OUR_MISSION = "our_mission";
+    case OUR_VALUE = "our_value";
 }
