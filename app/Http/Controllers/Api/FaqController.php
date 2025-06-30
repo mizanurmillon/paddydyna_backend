dd69d4bfd680be8ba2b059d016e78718b9a3a269
mizanurmillon <mr7517218@gmail.com>
2025-06-30 12:19:30 +0600

    faqs issue fix it

--- app/Http/Controllers/Api/FaqController.php
+++ app/Http/Controllers/Api/FaqController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enum\Page;
+use App\Models\Cms;
 use App\Models\Faq;
+use App\Enum\PageSection;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -15,10 +18,22 @@ class FaqController extends Controller
     {
         $data = Faq::where('status', 'active')->get();
 
+        $faq_header_section = Cms::query()
+                            ->select('title', 'sub_title')
+                            ->where('page_name',  Page::HOME->value)
+                            ->where('section_name', PageSection::ABOUT_SECTION->value)
+                            ->where('status', 'active')
+                            ->first();
+
         if ($data->isEmpty()) {
             return $this->error([], 'Faq not found', 200);
         }
 
-        return $this->success($data, 'Faq fetch Successful!', 200);
+        $response = [
+            'title' => $faq_header_section->title,
+            'sub_title' => $faq_header_section->sub_title,
+            'faqs' => $data
+        ];
+        return $this->success($response, 'Faq fetch Successful!', 200);
     }
 }
